8ba6cdf0100fa5ff237f1f816eee290874688b96
Yvette <yvettecook@gmail.com>
2015-12-28 17:48:17 +0000

    End of Part 1

---
+++ FizzBuzz/Brain.swift
@@ -0,0 +1,41 @@
+//
+//  Brain.swift
+//  FizzBuzz
+//
+//  Created by Yvette Cook on 28/12/2015.
+//  Copyright © 2015 Yvette. All rights reserved.
+//
+
+import Foundation
+
+class Brain: NSObject {
+    
+    func isDivisibleByThree(number: Int) -> Bool {
+        return isDivisibleBy(3, number: number)
+    }
+    
+    func isDivisibleByFive(number: Int) -> Bool {
+        return isDivisibleBy(5, number: number)
+    }
+    
+    func isDivisibleByFifteen(number: Int) -> Bool {
+        return isDivisibleBy(15, number: number)
+    }
+    
+    func isDivisibleBy(divisor: Int, number: Int) -> Bool {
+        return number % divisor == 0
+    }
+    
+    func check(number: Int) -> String {
+        if isDivisibleByFifteen(number) {
+            return "FizzBuzz"
+        } else if isDivisibleByThree(number) {
+            return "Fizz"
+        } else if isDivisibleByFive(number){
+            return "Buzz"
+        } else {
+            return "\(number)"
+        }
+    }
+    
+}
---
+++ FizzBuzzTests/BrainTests.swift
@@ -0,0 +1,76 @@
+//
+//  BrainTests.swift
+//  FizzBuzz
+//
+//  Created by Yvette Cook on 28/12/2015.
+//  Copyright © 2015 Yvette. All rights reserved.
+//
+
+import XCTest
+@testable import FizzBuzz
+
+class BrainTests: XCTestCase {
+    
+    let brain = Brain()
+
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+
+    func testIsDivisibleByThree() {
+        let result = brain.isDivisibleByThree(3)
+        XCTAssertEqual(result, true)
+    }
+
+    func testIsNotDivisibleByThree() {
+        let result = brain.isDivisibleByThree(1)
+        XCTAssertEqual(result, false)
+    }
+    
+    func testIsDivisibleByFive() {
+        let result = brain.isDivisibleByFive(5)
+        XCTAssertEqual(result, true)
+    }
+    
+    func testIsNotDivisibleByFive() {
+        let result = brain.isDivisibleByFive(1)
+        XCTAssertEqual(result, false)
+    }    
+
+    func testIsDivisibleByFifteen() {
+        let result = brain.isDivisibleByFifteen(15)
+        XCTAssertEqual(result, true)
+    }
+    
+    func testIsNotDivisibleByFifteen() {
+        let result = brain.isDivisibleByFifteen(1)
+        XCTAssertEqual(result, false)
+    }
+    
+    func testSayFizz() {
+        let result = brain.check(3)
+        XCTAssertEqual(result, "Fizz")
+    }
+    
+    func testSayBuzz() {
+        let result = brain.check(5)
+        XCTAssertEqual(result, "Buzz")
+    }
+    
+    func testSayFizzBuzz() {
+        let result = brain.check(15)
+        XCTAssertEqual(result, "FizzBuzz")
+    }
+    
+    func testSayNumber() {
+        let result = brain.check(1)
+        XCTAssertEqual(result, "1")
+    }
+    
+}
